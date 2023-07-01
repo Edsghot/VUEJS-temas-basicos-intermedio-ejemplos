31c141bde244048e707ee755bb5ce67cd1731392
Ed Soto <edsghot@gmail.com>
2023-07-01 16:42:22 -0500

    Agregando como usar v-model o reactividad con input

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+    import { ref } from 'vue';
+    const nuevaFruta = ref("");
+    
+    const agregar=()=>{
+        console.log(nuevaFruta.value)
+    }
+
+</script>
+
+
+<template>
+    <input type="text" v-model="nuevaFruta">
+    <br/>
+    <button @click="agregar"> agregar</button>
+    <h1>{{ nuevaFruta }}</h1>
+</template>
